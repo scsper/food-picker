45f866bb50495a6e2a5582d23311486cdf82dcdb
Scott Sperling <scsper@gmail.com>
2016-04-26 10:25:45 -0700

    Add basic launch

--- src/browser/launch.js
+++ src/browser/launch.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/app.jsx';
+
+export default function() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('container')
+    );
+}
