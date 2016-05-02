714bfd0401255b2b3dc46139c4e96cfa7df2d1fa
Scott Sperling <scsper@gmail.com>
2016-05-01 18:47:05 -0700

    Fix port.

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,8 @@
 var app = require('../src/server/app');
+var DEFAULT_PORT = 3000;
 
-app.listen('3000', function() {
-    console.log('Listening to port 3000');
+app.set('port', (process.env.PORT || DEFAULT_PORT));
+
+app.listen(app.get('port'), function() {
+    console.log('listening for connection on port ' + app.get('port'));
 });
